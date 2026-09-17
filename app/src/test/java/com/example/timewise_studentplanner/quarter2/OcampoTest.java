37797e168d09c39332b3df4cf3d80ea504159d82
ashenbasilio-dot <ashen.basilio@gmail.com>
2026-09-17 18:25:31 +0800

    add code in my own java class

---
+++ app/src/test/java/com/example/timewise_studentplanner/quarter2/OcampoTest.java
@@ -0,0 +1,20 @@
+package com.example.timewise_studentplanner.quarter2;
+
+import org.junit.Test;
+
+public class OcampoTest {
+    @Test
+    public void printMyProfile() {
+        // --- 1. THE INPUT (storing your personal details in variables) ---
+        String myName = "Rayshan";
+        String petName = "Bornok";
+        String favFood = "Sinigang na baboy";
+        int myAge = 17;
+
+        // --- 2. THE OUTPUT (Printing to the console) ---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
+}
